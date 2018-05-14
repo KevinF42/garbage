941becef09b455b0e85c8cfb5cb8116e323bf7dc
KevinF42 <kforrister@segfault.com>
2018-05-14 10:29:57 -0700

    Add files via upload

---
+++ form/phpform/phpform/db/db.php
@@ -0,0 +1,12 @@
+<?php
+   $hostname = "localhost";
+   $database = "stream";
+   $username = "***********";
+   $password = "*******
+  	function clean($input, $maxlength)
+ 	{
+     $input = substr($input, 0, $maxlength);
+    $input = EscapeShellCmd($input);
+   return ($input);
+  }
+?>
