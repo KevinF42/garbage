a22d93b154e1a72d0482e6ee83b59b1e0a9b2c7c
KJF <kforrister@segfault.com>
2018-05-17 08:29:05 -0700

    aa

--- app/README.md
+++
@@ -1,4 +0,0 @@
-# php-app
-http://www.codingcage.com/ is the source of this code.
-
-Version 1.0
--- form/phpform/phpform/db/error.php
+++
@@ -1,7 +0,0 @@
-<?php
-function showerror()
-	{	
-		//die("<h1>Input error. . Return to <a href = http://10.183.0.0/> HOME</a>");
-		die("Input error. .".mysql_errno(). " : ".mysql_error());
-	}
-?>
