d3ea3f0f03a4233eaa9c8cf7dc36391ca2c3bcbb
KevinF42 <kforrister@segfault.com>
2018-05-22 08:39:11 -0700

    Add files via upload

---
+++ CTF/Start/42/Confirmed/Ghostbusters/Dangerzone/Back/WhyisGamora/Desire/Blue/Megaman/Delete/Ram/Ironman/Parzival/Hamilton/Crazytrain/ZarduHasselfrau/JasonBourne/Vote/Midnight/Protoman/Countingstars/Thriftshop/End/index.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="../../../../../../../../../../../../../../../../../../../../../../../../../css/custom.css">
+
+<div class="header">
+   <h1>KevinF.IOE</h1>				
+</div>
+
+<div class="row">
+
+<div class="col-3 menu">
+  <ul>
+<li><a href="">Congratulations! You have finished this test and have 
+captured the flag. Litterally! Click on the flag to download it, and 
+feel free to brag if you are second or third. (Yes. I know I made it 
+but I also finished it first.) Congratulations. And please enjoy this 
+flag.</a></li>
+  </ul>
+</div>
+
+<div class="col-6">
+	<h1>  
+   <img src="flag.jpeg"width="320" height="320">
+  </h1>
+<div class="footer">
+  <p>Welcome to My site!</p>
+</div>
